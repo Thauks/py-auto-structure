0674f8394329e6dce18038271aa2db75bce5769d
Jordi Mestres <jordimestrees@gmail.com>
2024-01-19 18:36:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme_file:
 
 setup(
     name='py-auto-structure',
-    version='0.1',
+    version='0.1.2',
     author='Thauks',
     description='A tool to create a directory structure based on a YAML file',
     long_description=long_description,
@@ -14,6 +14,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pyyaml',
+        'wheel',
+        'setuptools'
     ],
     entry_points={
         'console_scripts': [
